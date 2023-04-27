c19bba9f33acb39aa5d27314830478cc05fee214
Sarika Khot <2000094432@hexaware.com>
2023-04-27 15:13:03 +0530

    Commit Implementation of service and repo

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService: ICourseService
+    {
+        private ICoursesRepository _courseRepository;
+
+        public CourseService(ICoursesRepository coursesRepository)
+        {
+            _courseRepository = coursesRepository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
